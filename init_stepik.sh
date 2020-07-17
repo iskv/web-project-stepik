32c169c97f871fa47c6e2bf82b762520d85dca1b
skvich <ivaleons@gmail.com>
2020-07-17 19:59:33 +0300

    fix

--- init_stepik.sh
+++ init_stepik.sh
@@ -1,11 +1,10 @@
 # Configuring nginx
 sudo ln -sf /home/box/web/etc/nginx_stepik.conf /etc/nginx/sites-enabled/test.conf
-sudo rm -rf /etc/nginx/sites-enabled/default
 # Restart nginx
 sudo /etc/init.d/nginx stop
 sudo /etc/init.d/nginx start
 # Run gunicorn
 cd ~/web
-gunicorn -b 0.0.0.0:8080 hello:app --daemon
+gunicorn -b 0.0.0.0:8080 --access-logfile acc_8080.log hello:app --daemon
 cd ~/web/ask
-gunicorn -b 0.0.0.0:8000 ask.wsgi --daemon
+gunicorn -b 0.0.0.0:8000 --access-logfile acc_8000.log ask.wsgi --daemon
