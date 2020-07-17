1aff9f0f6b70d4b84be1f721540f8cde905bdcaf
skvich <ivaleons@gmail.com>
2020-07-17 20:42:45 +0300

    fix

--- init_wsl.sh
+++ init_wsl.sh
@@ -1,4 +1,4 @@
-# Configuring nginx; change 'conf.d' to 'sites-enabled' and 'ivan' to 'box' for stepic
+# Configuring nginx
 sudo ln -sf /home/ivan/web/etc/nginx_wsl.conf /etc/nginx/conf.d/test.conf
 # Restart nginx
 sudo /etc/init.d/nginx stop
