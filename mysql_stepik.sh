a00a7a45994bf1b663a2f1b1def138b60395d017
skvich <ivaleons@gmail.com>
2020-07-29 15:49:43 +0300

    update

--- mysql_stepik.sh
+++ mysql_stepik.sh
@@ -5,4 +5,4 @@ mysql -uroot -e "CREATE USER 'box'@'localhost' IDENTIFIED BY 'password';"
 mysql -uroot -e "GRANT ALL PRIVILEGES ON ask.* TO 'box'@'localhost' WITH GRANT OPTION;"
 # Django MySQL migrations
 python3 ~/web/ask/manage.py makemigrations
-python3 ~/web/ask/manage.py migrate
+python3 ~/web/ask/manage.py migrategitgit
