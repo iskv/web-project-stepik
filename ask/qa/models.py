76deb6e82efe9f17c9e6d386af5abb62c4bfe0f4
skvich <ivaleons@gmail.com>
2020-07-29 17:00:16 +0300

    update

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -22,4 +22,4 @@ class Answer(models.Model):
     text = models.TextField()
     added_at = models.DateTimeField(auto_now_add=True)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    author = models.CharField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
