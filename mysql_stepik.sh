880184f133c33c805829701fc3bdbd7e38ad59c5
skvich <ivaleons@gmail.com>
2020-07-29 16:09:35 +0300

    update

--- mysql_stepik.sh
+++ mysql_stepik.sh
@@ -1,3 +1,4 @@
+sudo apt-get install default-libmysqlclient-dev build-essential
 sudo -H pip3 install mysqlclient
 sudo /etc/init.d/mysql start
 mysql -uroot -e "CREATE DATABASE ask CHARACTER SET utf8;"
@@ -5,4 +6,4 @@ mysql -uroot -e "CREATE USER 'box'@'localhost' IDENTIFIED BY 'password';"
 mysql -uroot -e "GRANT ALL PRIVILEGES ON ask.* TO 'box'@'localhost' WITH GRANT OPTION;"
 # Django MySQL migrations
 python3 ~/web/ask/manage.py makemigrations
-python3 ~/web/ask/manage.py migrategitgit
+python3 ~/web/ask/manage.py migrate
