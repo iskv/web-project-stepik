f68048ff26d06504f8a4d1e752609970ffeecf97
skvich <ivaleons@gmail.com>
2020-07-29 16:52:26 +0300

    update

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Questions(models.Model):
+class Question(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
     added_at = models.DateTimeField(auto_now_add=True)
@@ -13,7 +13,7 @@ class Questions(models.Model):
 class Answer(models.Model):
     text = models.TextField()
     added_at = models.DateTimeField(auto_now_add=True)
-    question = models.ForeignKey(Questions, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
     author = models.CharField(max_length=255)
 
 
